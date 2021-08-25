2dc22badc60c5a1a9570091cce1542e7f871d313
Joao Victor <joaogmavictor@gmail.com>
2021-08-25 11:24:41 -0300

    Concluindo Visual Skills

--- src/Components/HardSkills/Skills.jsx
+++ src/Components/HardSkills/Skills.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { DiSass, DiCss3, DiJavascript1 } from 'react-icons/di';
 import { FaReact, FaHtml5, FaBootstrap } from 'react-icons/fa';
-import Tilt from 'react-tilt';
 import List from './Component/List';
 import LottieAnimation from '../Animation/Lottie';
 import animation from '../Animation/Skilss.json';
 
 export default function Skills() {
+  function animeScrolls() {
+    const target = document.querySelectorAll('[data-anime]');
+    console.log(target);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach((element) => {
+      if ((windowTop) > element.offsetTop) {
+        element.classList.add('animate');
+        console.log(element);
+      } else {
+        element.classList.remove('animate');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animeScrolls);
+
   return (
     <div className="skills-container">
-      <Tilt options={ { max: 50 } }>
-        <h1 className="skills-container-title">Skills</h1>
-      </Tilt>
+      <h1 className="skills-container-title" data-anime="anime">Skills</h1>
       <div className="skills-container-main">
-        <ul className="skills-container-ul">
+        <ul className="skills-container-ul" data-anime="anime">
           <List
             icon={ <DiSass className="icons" /> }
             title="Sass"
