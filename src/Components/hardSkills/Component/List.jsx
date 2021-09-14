fa4b23afd5f0d4713e1fbb5da5619cd613f213b4
Joao Victor <joaogmavictor@gmail.com>
2021-09-14 14:46:55 -0300

    Mudando List

--- src/Components/hardSkills/Component/List.jsx
+++ src/Components/hardSkills/Component/List.jsx
@@ -1,20 +1,17 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from 'react';
+import React from 'react';
 import Tilt from 'react-tilt';
 
-export default class List extends Component {
-  render() {
-    const { icon, title } = this.props;
-    return (
-      <Tilt
-        className="Tilt skills-container-list"
-        options={ { max: 50, reverse: false, scale: 1.2 } }
-      >
-        <li className="Tilt-inner">
-          {icon}
-          <h3>{title}</h3>
-        </li>
-      </Tilt>
-    );
-  }
-}
+const List = ({ icon, title }) => (
+  <Tilt
+    className="Tilt skills-container-list"
+    options={ { max: 50, reverse: false, scale: 1.2 } }
+  >
+    <li className="Tilt-inner">
+      {icon}
+      <h3>{title}</h3>
+    </li>
+  </Tilt>
+);
+
+export default List;
